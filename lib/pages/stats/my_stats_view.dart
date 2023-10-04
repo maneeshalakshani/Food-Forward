2a30e0d092ae5323f9309bc33c14184e0a46bf69
maneeshalakshani <maneeshalakshani9@gmail.com>
2023-10-04 19:30:49 +0530

    feat: my stats view

--- lib/pages/stats/my_stats_view.dart
+++ lib/pages/stats/my_stats_view.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:food_forward/const.dart';
 import 'package:food_forward/pages/Components/appbar.dart';
+import 'package:food_forward/pages/Components/clickable_container.dart';
 
 class MyStatsView extends HookWidget {
   const MyStatsView({
@@ -10,15 +12,55 @@ class MyStatsView extends HookWidget {
 
   @override
   Widget build(BuildContext context) {
-    double height = MediaQuery.of(context).size.height;
     double width = MediaQuery.of(context).size.width;
     return Scaffold(
       appBar: appBar(),
       body: SingleChildScrollView(
-        child: Column(
-          children: [
-            Text("sgg"),
-          ],
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 20),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const Padding(
+                padding: EdgeInsets.only(bottom: 10),
+                child: Text(
+                  "My Stats",
+                  style: TextStyle(
+                    fontSize: 25,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+              const Text(
+                "Small act, big impact",
+                style: TextStyle(
+                  fontSize: 16,
+                ),
+              ),
+              Center(
+                child: Image.asset("assets/home.jpg",),
+              ),
+              ClickableContainer(
+                text: "Total Donations",
+                width: width,
+                bgColor: LIGHT_PINK,
+                textColor: BLACK,
+              ),
+              ClickableContainer(
+                text: "Community Contributions",
+                width: width,
+                bgColor: LIGHT_PINK,
+                textColor: BLACK,
+              ),
+              ClickableContainer(
+                text: "Environmental Impact",
+                width: width,
+                bgColor: LIGHT_PINK,
+                textColor: BLACK,
+              ),
+            ],
+          ),
         ),
       ),
     );
