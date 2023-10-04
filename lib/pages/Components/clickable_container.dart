ad3403021cfce3acb5ea6961ca22b8009d669da3
maneeshalakshani <maneeshalakshani9@gmail.com>
2023-10-04 18:17:46 +0530

    feat: img to clickable container

--- lib/pages/Components/clickable_container.dart
+++ lib/pages/Components/clickable_container.dart
@@ -2,7 +2,6 @@ import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:food_forward/const.dart';
-import 'package:food_forward/routes/routes.gr.dart';
 
 class ClickableContainer extends HookWidget {
   const ClickableContainer({
@@ -28,7 +27,7 @@ class ClickableContainer extends HookWidget {
   Widget build(BuildContext context) {
     return GestureDetector(
       onTap: (){
-        context.router.push(ProfileRoute());
+        context.router.push(route);
       },
       child: Card(
         color: bgColor ?? BLACK,
