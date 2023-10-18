5c9b7b6ed352a30434add9a4d64361e90ce5cef8
maneeshalakshani <maneeshalakshani9@gmail.com>
2023-10-18 17:28:23 +0530

    feat: volunteer add tasks

--- lib/pages/volunteer/notifications/notification_card.dart
+++ lib/pages/volunteer/notifications/notification_card.dart
@@ -1,28 +1,27 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:food_forward/models/Order.dart';
 import 'package:food_forward/pages/Components/squareButton.dart';
+import 'package:food_forward/routes/routes.gr.dart';
+import 'package:food_forward/services/volunteer/volunteer_services.dart';
 
 class NotificationCard extends HookWidget {
   const NotificationCard({
     Key? key, 
-    required this.orderNo,
-    required this.totalPrice,
-    required this.seeLocationOnTap,
-    required this.acceptOnPress,
-    required this.rejectOnPress,
+    required this.context,
+    required this.data,
   }) : super(key: key);
-  final String orderNo;
-  final double totalPrice;
-  final void Function()? seeLocationOnTap;
-  final void Function()? acceptOnPress;
-  final void Function()? rejectOnPress;
+  final BuildContext context;
+  final DocumentSnapshot data;
 
   @override
   Widget build(BuildContext context) {
     double width = MediaQuery.of(context).size.width;
+    final order = OrderClass.fromSnapshot(data);
 
     return Card(
-      color: Color.fromARGB(255, 236, 236, 236),
+      color: const Color.fromARGB(255, 236, 236, 236),
       child: Column(
         children: [
           Row(
@@ -41,21 +40,21 @@ class NotificationCard extends HookWidget {
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
                     Text(
-                      "Order No: $orderNo",
+                      "Order No: ${data.id}",
                       style: const TextStyle(
                         color: Colors.red,
                         fontWeight: FontWeight.bold,
                       ),
                     ),
                     Text(
-                      "Total Price: $totalPrice",
+                      "Total Price: ${calculateTotalPrice(order: order)}",
                       style: const TextStyle(
                         color: Colors.black,
                         fontWeight: FontWeight.bold,
                       ),
                     ),
                     InkWell(
-                      onTap: seeLocationOnTap,
+                      onTap: (){},
                       child: const Text(
                         "See Location",
                       ),
@@ -69,25 +68,43 @@ class NotificationCard extends HookWidget {
             mainAxisAlignment: MainAxisAlignment.spaceAround,
             children: [
               SquareButton(
-                onPressed: acceptOnPress, 
+                onPressed: (){
+                  VolunteerFunctions().createMyTask(
+                    userId: "gJTFh23K", 
+                    orderId: data.id, 
+                    context: context, 
+                    route: const VolunteerProfileRoute(), 
+                    taskStatus: "Pending",
+                    latitude: order.latitude,
+                    longitude: order.longitude,
+                  );
+                }, 
                 text: "Accept",
                 btnWidth: 100,
                 btnColor: Colors.green,
                 btnPadding: const EdgeInsets.symmetric(vertical: 0),
                 btnMargin: const EdgeInsets.symmetric(vertical: 10),
               ),
-              SquareButton(
-                onPressed: rejectOnPress, 
-                text: "Decline",
-                btnColor: Colors.red,
-                btnWidth: 100,
-                btnPadding: const EdgeInsets.symmetric(vertical: 0),
-                btnMargin: const EdgeInsets.symmetric(vertical: 10),
-              )
+              // SquareButton(
+              //   onPressed: rejectOnPress, 
+              //   text: "Decline",
+              //   btnColor: Colors.red,
+              //   btnWidth: 100,
+              //   btnPadding: const EdgeInsets.symmetric(vertical: 0),
+              //   btnMargin: const EdgeInsets.symmetric(vertical: 10),
+              // )
             ],
           )
         ],
       ),
     );
+  }
+
+  calculateTotalPrice({required OrderClass order}){
+    double total = 0.0;
+    for(int i=0; i<order.cartItems.length; i++){
+      total = total + double.parse(order.cartItems[i].food.price) * double.parse(order.cartItems[i].noOfItems.toString());
+    }
+    return total;
   }
 }
