285b01aca801b8e01daad8eeb264e49599beb692
maneeshalakshani <maneeshalakshani9@gmail.com>
2023-10-20 23:31:04 +0530

    fix: total, go back explorer

--- lib/pages/recipient/cart/cart_view.dart
+++ lib/pages/recipient/cart/cart_view.dart
@@ -1,3 +1,4 @@
+import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 import 'package:food_forward/models/Cart.dart';
 import 'package:food_forward/pages/Components/appbar.dart';
@@ -22,6 +23,7 @@ class _CartViewState extends State<CartView> {
   Widget build(BuildContext context) {
     double height = MediaQuery.of(context).size.height;
     double width = MediaQuery.of(context).size.width;
+    double total = calculateTotal();
 
     return Scaffold(
       appBar: appBar(),
@@ -30,7 +32,7 @@ class _CartViewState extends State<CartView> {
         child: Column(
           children: [
             Container(
-              height: height/10*7,
+              height: height/10*6,
               width: width,
               margin: const EdgeInsets.symmetric(horizontal: 25, vertical: 20),
               child: ListView.builder(
@@ -53,6 +55,13 @@ class _CartViewState extends State<CartView> {
                 },
               ),
             ),
+            Text(
+              "Total: Rs.${total.toStringAsFixed(2)}",
+              style: const TextStyle(
+                fontSize: 18,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
             SquareButton(
               onPressed: (){
                 RecipientFunction().createOrder(
@@ -61,13 +70,27 @@ class _CartViewState extends State<CartView> {
                   context: context, 
                   route: OrderConfirmRoute(orderNo: "afc7687JJe3"),
                 );
-                // context.router.push(OrderConfirmRoute(orderNo: "afc7687JJe3"));
               }, 
               text: "Place Order"
+            ),
+            SquareButton(
+              onPressed: (){
+                context.router.pop();
+              }, 
+              text: "Go Back to Explorer",
+              btnMargin: const EdgeInsets.only(top: 0),
             )
           ],
         ),
       ),
     );
+  }
+
+  double calculateTotal() {
+    double total = 0;
+    for (var item in widget.cart.items) {
+      total += double.parse(item.food.price) * item.noOfItems;
+    }
+    return total;
   }
 }
