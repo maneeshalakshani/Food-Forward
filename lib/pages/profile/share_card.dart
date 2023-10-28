76a7b85c81b2a0bf6866f482a1e98e4a5e7607fc
maneeshalakshani <maneeshalakshani9@gmail.com>
2023-10-28 14:06:52 +0530

    feat: instagram share

--- lib/pages/profile/share_card.dart
+++ lib/pages/profile/share_card.dart
@@ -1,6 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:flutter_social_content_share/flutter_social_content_share.dart';
 import 'package:food_forward/const.dart';
 import 'package:food_forward/models/Badge.dart';
 
@@ -49,7 +50,14 @@ class ShareCard extends HookWidget {
         Padding(
           padding: const EdgeInsets.only(right: 8),
           child: InkWell(
-            onTap: (){},
+            onTap: (){
+              FlutterSocialContentShare.share(
+                type: ShareType.instagramWithImageUrl,
+                imageUrl: "https://firebasestorage.googleapis.com/v0/b/food-forward-3a32b.appspot.com/o/badges.png?alt=media&token=19484bb2-727d-4094-bf50-ba6a6fd1f2c8&_gl=1*1cbfslv*_ga*MTY1MTk2NTQyLjE2OTcwNDc2OTM.*_ga_CW55HF8NVT*MTY5ODQ4MjAxNy42Ni4xLjE2OTg0ODIwNzUuMi4wLjA.",
+                quote: badge.name,
+                imageName: badge.name
+              );
+            },
             child: const Text(
               "Share",
               style: TextStyle(
